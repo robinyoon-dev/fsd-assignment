acc71a1940704a7aa010fd88f5d6c1fc0aa92a09
Robin Yoon <robinyoonofficial@gmail.com>
2026-01-15 13:06:53 +0900

    chore: FSD 아카텍처로 디렉토리 구조 변경

---
+++ weather-project/src/app/app.tsx
@@ -0,0 +1,9 @@
+import { HomePage } from '../pages/home'
+
+export function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-50">
+      <HomePage />
+    </div>
+  )
+}
